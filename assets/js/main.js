fc09f87d55cfd7cf74b54a4c2e0d59cb0d723b22
shortlistweb <77305024+shortlistweb@users.noreply.github.com>
2021-01-25 22:55:52 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,119 +50,18 @@ function saveMessage(name, number, email, message) {
 }
 
 !(function ($) {
+
   "use strict";
 
   // Toggle .header-scrolled class to #header when page is scrolled
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('#header').addClass('header-scrolled');
-    } else {
-      $('#header').removeClass('header-scrolled');
-    }
-  });
 
-  if ($(window).scrollTop() > 100) {
-    $('#header').addClass('header-scrolled');
-  }
-
-  // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $('#header').outerHeight() - 1;
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function (e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        e.preventDefault();
-        var scrollto = target.offset().top - scrolltoOffset;
-
-        if ($(this).attr("href") == '#header') {
-          scrollto = 0;
-        }
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-        if ($(this).parents('.nav-menu, .mobile-nav').length) {
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
-        }
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
-        }
-        return false;
-      }
-    }
-  });
-
-  // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function () {
-    if (window.location.hash) {
-      var initial_nav = window.location.hash;
-      if ($(initial_nav).length) {
-        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-      }
-    }
-  });
 
   // Mobile Navigation
-  // if ($('.nav-menu').length) {
-  //   var $mobile_nav = $('.nav-menu').clone().prop({
-  //     class: 'mobile-nav d-lg-none'
-  //   });
-  //   $('body').append($mobile_nav);
-  //   $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-  //   $('body').append('<div class="mobile-nav-overly"></div>');
-  //   $(document).on('click', '.mobile-nav-toggle', function (e) {
-  //     $('body').toggleClass('mobile-nav-active');
-  //     $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-  //     $('.mobile-nav-overly').toggle();
-  //   });
-  //   $(document).on('click', '.mobile-nav .drop-down > a', function (e) {
-  //     e.preventDefault();
-  //     $(this).next().slideToggle(300);
-  //     $(this).parent().toggleClass('active');
-  //   });
-  //   $(document).click(function (e) {
-  //     var container = $(".mobile-nav, .mobile-nav-toggle");
-  //     if (!container.is(e.target) && container.has(e.target).length === 0) {
-  //       if ($('body').hasClass('mobile-nav-active')) {
-  //         $('body').removeClass('mobile-nav-active');
-  //         $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-  //         $('.mobile-nav-overly').fadeOut();
-  //       }
-  //     }
-  //   });
-  // } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-  //   $(".mobile-nav, .mobile-nav-toggle").hide();
-  // }
-  $('.navbar-nav>li>a').on('click', function () {
+
+  $('.navbar-nav > a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
   });
-  // Navigation active state on scroll
-  // var nav_sections = $('section');
-  // var main_nav = $('.nav-menu, .mobile-nav');
 
-  // $(window).on('scroll', function () {
-  //   var cur_pos = $(this).scrollTop() + 200;
-
-  //   nav_sections.each(function () {
-  //     var top = $(this).offset().top,
-  //       bottom = top + $(this).outerHeight();
-
-  //     if (cur_pos >= top && cur_pos <= bottom) {
-  //       if (cur_pos <= bottom) {
-  //         main_nav.find('li').removeClass('active');
-  //       }
-  //       main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-  //     }
-  //     if (cur_pos < 300) {
-  //       $(".nav-menu ul:first li:first").addClass('active');
-  //     }
-  //   });
-  // });
 
   // Back to top button
   $(window).scroll(function () {
@@ -178,8 +77,11 @@ function saveMessage(name, number, email, message) {
     }, 0, 'easeInOutExpo');
     return false;
   });
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
 
-
+  // Smooth scroll for the navigation menu and links with .scrollto classes
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
@@ -200,6 +102,32 @@ function saveMessage(name, number, email, message) {
   });
 
 })(jQuery);
+// Sticky Navbar
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 0) {
+    $('.navbar').addClass('nav-sticky');
+  } else {
+    $('.navbar').removeClass('nav-sticky');
+  }
+});
+
+
+// Smooth scrolling on the navbar links
+$(".navbar-nav a").on('click', function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top - 45
+    }, 0, 'easeInOutExpo');
+
+    if ($(this).parents('.navbar-nav').length) {
+      $('.navbar-nav .active').removeClass('active');
+      $(this).closest('a').addClass('active');
+    }
+  }
+});
+
 
 $('.testimonial-carousel').owlCarousel({
   loop: true,
@@ -224,3 +152,10 @@ $('.testimonial-carousel').owlCarousel({
 function clicked() {
   return confirm('submitted');
 }
+$('.nav a').click(function (e) {
+  e.preventDefault();
+  var $scrooll_to_id = $(this.getAttribute('href'));
+  $('html').stop(true).animate({
+    scrollTop: ($scrooll_to_id.position().top - $('.nav-bar').height())
+  });
+});
