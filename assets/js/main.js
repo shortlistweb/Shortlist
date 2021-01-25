5c8d7e9c998e2e044478ea0ba868079aeb356d6a
shortlistweb <77305024+shortlistweb@users.noreply.github.com>
2021-01-25 20:50:39 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,37 +108,39 @@ function saveMessage(name, number, email, message) {
   });
 
   // Mobile Navigation
-  if ($('.nav-menu').length) {
-    var $mobile_nav = $('.nav-menu').clone().prop({
-      class: 'mobile-nav d-lg-none'
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-    $('body').append('<div class="mobile-nav-overly"></div>');
-    $(document).on('click', '.mobile-nav-toggle', function (e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-      $('.mobile-nav-overly').toggle();
-    });
-    $(document).on('click', '.mobile-nav .drop-down > a', function (e) {
-      e.preventDefault();
-      $(this).next().slideToggle(300);
-      $(this).parent().toggleClass('active');
-    });
-    $(document).click(function (e) {
-      var container = $(".mobile-nav, .mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
-        }
-      }
-    });
-  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-    $(".mobile-nav, .mobile-nav-toggle").hide();
-  }
-
+  // if ($('.nav-menu').length) {
+  //   var $mobile_nav = $('.nav-menu').clone().prop({
+  //     class: 'mobile-nav d-lg-none'
+  //   });
+  //   $('body').append($mobile_nav);
+  //   $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+  //   $('body').append('<div class="mobile-nav-overly"></div>');
+  //   $(document).on('click', '.mobile-nav-toggle', function (e) {
+  //     $('body').toggleClass('mobile-nav-active');
+  //     $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  //     $('.mobile-nav-overly').toggle();
+  //   });
+  //   $(document).on('click', '.mobile-nav .drop-down > a', function (e) {
+  //     e.preventDefault();
+  //     $(this).next().slideToggle(300);
+  //     $(this).parent().toggleClass('active');
+  //   });
+  //   $(document).click(function (e) {
+  //     var container = $(".mobile-nav, .mobile-nav-toggle");
+  //     if (!container.is(e.target) && container.has(e.target).length === 0) {
+  //       if ($('body').hasClass('mobile-nav-active')) {
+  //         $('body').removeClass('mobile-nav-active');
+  //         $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  //         $('.mobile-nav-overly').fadeOut();
+  //       }
+  //     }
+  //   });
+  // } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+  //   $(".mobile-nav, .mobile-nav-toggle").hide();
+  // }
+  $('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+  });
   // Navigation active state on scroll
   // var nav_sections = $('section');
   // var main_nav = $('.nav-menu, .mobile-nav');
@@ -173,7 +175,7 @@ function saveMessage(name, number, email, message) {
   $('.back-to-top').click(function () {
     $('html, body').animate({
       scrollTop: 0
-    }, 1500, 'easeInOutExpo');
+    }, 0, 'easeInOutExpo');
     return false;
   });
 
